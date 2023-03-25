946dbdf2782496d3a11c87a0d58e5a3df8b02d34
sulfaratnasari <sulfaratnasari@gmail.com>
2023-03-25 17:32:47 +0700

    add swagger

--- start/routes.js
+++ start/routes.js
@@ -26,4 +26,121 @@ Route.get('/employee/delete/:id', 'EmployeeController.delete').as('employee.dele
 Route.get('/employee/view/:id', 'EmployeeController.detail').as('employee.detail')
 Route.get('login', 'Auth/LoginController.index').as('login.index')
 Route.post('login', 'Auth/LoginController.check').as('login.check')
-Route.get('logout', 'Auth/LoginController.logout').as('logout')
+Route.get('/logout', 'Auth/LoginController.logout').as('logout')
+
+//API
+
+/**
+* @swagger
+* /auth/login:
+*   post:
+*     tags:
+*       - Auth
+*     summary: Get bearer token
+*     parameters:
+*       - name: body
+*         description: Employee email and password
+*         in: body
+*         required: true
+*         schema:
+*           type: object
+*           required:
+*            - email
+*           properties:
+*             email:
+*               type: string
+*             password:
+*               type: string
+*     responses:
+*       200:
+*         description: Success
+*/
+Route.post('/auth/login', 'Auth/LoginController.login')
+Route.group(() => {
+
+	/**
+  * @swagger
+  * /api/employee/view/:id:
+  *  get:
+  *     tags:
+  *       - Employee
+  *     security:
+  *        - bearerAuth: []
+  *     summary: Get employee detail
+  *     parameters:
+  *       - name: id
+  *         description: Employee ID
+  *         in: path
+  *         required: true
+  *         type: int
+  *     responses:
+  *       200:
+  *         description: Success
+  *         example:
+  *           name: employee_name
+  *           email: employee@mail.com
+  */
+	Route.get('/employee/view/:id', 'EmployeeController.detailApi').middleware(['auth:jwt'])
+	Route.delete('/employee/delete/:id', 'EmployeeController.deleteApi').middleware(['auth:jwt'])
+	Route.put('/employee/update/:id', 'EmployeeController.updateApi').middleware(['auth:jwt'])
+
+	/**
+* @swagger
+* /api/employee/store:
+*  get:
+*     tags:
+*       - Employee
+*     security:
+*        - bearerAuth: []
+*     summary: Add new
+parameters:
+*       - name: body
+*         description: Employee data
+*         in: body
+*         required: true
+*         schema:
+*           type: object
+*           required:
+*            - name
+*           properties:
+*             name:
+*               type: string
+*             email:
+*               type: string
+*	          role:
+*               type: string
+*	          address:
+*               type: string
+*	          directorate:
+*               type: string
+*     responses:
+*       200:
+*         description: Success
+*         example:
+*           name: employee_name
+*           email: employee@mail.com
+*/
+	Route.post('/employee/store', 'EmployeeController.storeApi').middleware(['auth:jwt'])
+
+	/**
+  * @swagger
+  * /api/employee/list:
+  *  get:
+  *     tags:
+  *       - Employee
+  *     security:
+  *        - bearerAuth: []
+  *     summary: Get employee list
+  *     responses:
+  *       200:
+  *         description: Success
+  * 		example:
+  *           - id: 1
+  *             name: user1
+  *             email: example@mail.com
+  *             role: Product Manager
+  *             directorate: Technology
+  *             address: Jakarta
+  */
+	Route.get('/employee/list', 'EmployeeController.indexApi').middleware(['auth:jwt'])
+}).prefix('/api')
